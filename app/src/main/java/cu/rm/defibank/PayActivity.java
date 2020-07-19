50eab29b1feefd7322239aba18a11c93803c0a08
Marcos Macias Sánchez <mmaciass940412@gmail.com>
2020-07-19 15:07:16 -0400

    add more pay

--- app/src/main/java/cu/rm/defibank/PayActivity.java
+++ app/src/main/java/cu/rm/defibank/PayActivity.java
@@ -1,14 +1,25 @@
 package cu.rm.defibank;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
-public class PayActivity extends AppCompatActivity {
+import cu.rm.defibank.parents.CustomActivityFullAnimated;
+
+public class PayActivity extends CustomActivityFullAnimated {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
+    }
+
+    @Override
+    protected void setOnClickListeners() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
