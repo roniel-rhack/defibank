b1eebe07322486c8c249bb4f7eb1930c032f29f0
Marcos Macias Sánchez <mmaciass940412@gmail.com>
2020-07-19 17:09:48 -0400

    change id and fix to fullscreen

--- app/src/main/java/cu/rm/defibank/parents/CustomActivity.java
+++ app/src/main/java/cu/rm/defibank/parents/CustomActivity.java
@@ -2,15 +2,26 @@ package cu.rm.defibank.parents;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * Una actividad personalizada para hacer mas guiada y sencilla la implementacion de las actividades
  */
 public abstract class CustomActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
 
     @Override
     public void setContentView(int layoutResID) {
