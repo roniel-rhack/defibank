3349078045023dd7276fd87c6787e06298116c02
Roniel <roniel.rack@gmail.com>
2020-07-24 08:20:37 -0400

    Update CheckMessages.java

--- app/src/main/java/cu/rm/defibank/utils/CheckMessages.java
+++ app/src/main/java/cu/rm/defibank/utils/CheckMessages.java
@@ -4,10 +4,11 @@ public class CheckMessages {
 
     /**
      * Devuelve un booleano que indica si el mensaje de autenticacion de etecsa
+     *
      * @param message El mensaje de autenticacion que ofrece etecsa a procesar
      * @return boolean Verdadero si el mensaje es satisfactorio
      */
-    public static boolean checkAutenticationMessage(String message){
-        return true;
+    public static boolean checkAutenticationMessage(String message) {
+        return message.contains("Usted se ha autenticado") && (message.contains("correctamente") || message.contains("satisfactoriamente"));
     }
 }
