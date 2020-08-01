8de4bb2ad80090e06f515b6825e3735a4a6b8db7
Marcos Macias Sánchez <mmaciass940412@gmail.com>
2020-07-31 20:48:48 -0400

    sum che3

--- app/src/main/java/cu/rm/defibank/utils/CheckMessages.java
+++ app/src/main/java/cu/rm/defibank/utils/CheckMessages.java
@@ -2,6 +2,10 @@ package cu.rm.defibank.utils;
 
 public class CheckMessages {
 
+    private static String Normilize(String text) {
+        return text.toLowerCase();
+    }
+
     /**
      * Devuelve un booleano que indica, según el mensaje de autenticación de etecsa, si la autenticación es satisfactoria o no
      *
@@ -9,16 +13,16 @@ public class CheckMessages {
      * @return boolean Verdadero si el mensaje es satisfactorio
      */
     public static boolean checkAuthenticationMessage(String message) {
-        return message.toLowerCase().contains("Usted se ha autenticado".toLowerCase());
+        return Normilize(message).contains(Normilize("Usted se ha autenticado"))
+                || Normilize(message).contains(Normilize("ya se encuentra autenticado"));
     }
 
     public static boolean checkTransferMethodMessage(String message) {
-        return message.toLowerCase().contains("realizado una transferencia".toLowerCase())
-                || message.toLowerCase().contains("Transferencia fue completada".toLowerCase());
+        return Normilize(message).contains(Normilize("realizado una transferencia"))
+                || Normilize(message).contains(Normilize("Transferencia fue completada"));
     }
 
-    public static boolean checkAddress(String address){
-        return address.toLowerCase().equals("PAGOxMOVIL".toLowerCase());
+    public static boolean checkAddress(String address) {
+        return Normilize(address).equals(Normilize("PAGOxMOVIL"));
     }
-
 }
